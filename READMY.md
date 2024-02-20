9f7fa83c9ee0659faea83a0242d7d02da67fb644
philippe <laurentiu.philippe@gmail.com>
2024-02-20 12:18:27 +0900

    add user model and tests

--- READMY.md
+++ READMY.md
@@ -8,3 +8,17 @@
 
 ### run tests
     docker-compose run --rm app sh -c "python manage.py test"
+
+### migrations
+    docker-compose run --rm app sh -c "python manage.py makemigrations"
+
+    docker-compose run --rm app sh -c "python manage.py migrate"
+
+
+### clear docker volume (clears database)
+    docker volume ls
+    docker volume rm recipe-app-api_dev-db-data
+
+
+### create super user
+    docker-compose run --rm app sh -c "python manage.py createsuperuser"
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,41 @@
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalised(self):
+        sample_emails = [
+            ['test1@example.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST2@EXAMPLE.COM', 'TEST2@example.com'],
+        ]
+
+        for email, exampled in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, exampled)
+
+    def test_new_user_without_emails_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
