23a6df274b3c4c85963dfc9c3483ccb6afee2a37
philippe <laurentiu.philippe@gmail.com>
2024-02-26 11:06:09 +0900

    add user update methode

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -111,6 +111,7 @@ class PublicUserApiTests(TestCase):
 
 
 class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication."""
 
     def setUp(self):
         self.user = create_user(
@@ -119,9 +120,11 @@ class PrivateUserApiTests(TestCase):
             name='Test Name',
         )
         self.client = APIClient()
-        self.client.force_authentice(user=self.user)
+        self.client.force_authenticate(user=self.user)
+
 
     def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user."""
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
@@ -131,16 +134,18 @@ class PrivateUserApiTests(TestCase):
         })
 
     def test_post_me_not_allowed(self):
+        """Test POST is not allowed for the me endpoint."""
         res = self.client.post(ME_URL, {})
 
-        self.assertEqual(res.status_code, status.HTTP_405_METHODE_NOT_ALLOWED)
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_update_user_profile(self):
-        payload = {'name': 'update name', 'password': 'newpassword123'}
+        """Test updating the user profile for the authenticated user."""
+        payload = {'name': 'Updated name', 'password': 'newpassword123'}
 
         res = self.client.patch(ME_URL, payload)
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
-        self.assertEqual(self.user.check_password(payload['password']))
+        self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
